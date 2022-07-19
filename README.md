98b65bcdf47e6e6789b2483a3049c7d776139883
Fuzzy Yeti <fuzzyyeti@github.com>
2022-07-19 00:32:25 -0600

    test script working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Example code for Grape.Art on-chain listing request program
+
+```bash
+solana airdrop --keypair ./keys/admin.json 10
+solana airdrop --keypair ./keys/user.json 10
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 
-//request 5Ph5jwoHwp24uVrgvUYLPX48Kmugzykfbm3Dhm7YXmzk verified 5hod5XM33BjqFBvKgsYKhzMzbkFHdsqJiF3q9BvqKQHq
 import {clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js"
 import {AnchorProvider, web3} from "@project-serum/anchor"
 import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet"
@@ -8,39 +7,52 @@ import {useAdmin, useListingRequest, useListingQuery, useManageAdmin} from 'grap
 import BN from "bn.js";
 
 
-const KEY_FILE = '/Users/fzzyyti/.config/solana/id.json'
-const CONFIG = '2xE9MyGe2PsXEyWivES2ESxi7iqm4L5cTAxj6C68eyQT'
-// 3pJHarWHeeToQM1g3J9cHkftJU8RWG5UYmG5ZAoHhHseYD5DeAGbCZcLwMmcMVFSkK1wq7xyoxcQFUD3mhz8r3mE
+
+const CONFIG = '6wrYrfryfjd3E523VkNU7cGycx4oF1K7LxK6HwLTYThQ'
+// New admin pubkey is D5hrpHhpp7TAxxzkrm41Jjx463cPaLZV8REze45pKFk7
 const main = async () => {
     const args = process.argv.slice(2);
     const connection = new Connection("http://127.0.0.1:8899")
     // const connection = new Connection("https://api.devnet.solana.com")
-    const keypair = await createKeypairFromFile(KEY_FILE)
+    const admin = './keys/new_admin.json'
+    const user = './keys/user.json'
+
+    const isAdmin = ['init','approve','deny', 'update_fee', 'new_admin'].includes(args[0])
+    const keypair = await createKeypairFromFile(isAdmin ? admin : user)
     const wallet = new NodeWallet(keypair)
     const provider = new AnchorProvider(connection, wallet, AnchorProvider.defaultOptions())
 
     switch (args[0]) {
         case "init":
-            const {updateAdmin, createConfig} = useManageAdmin(provider);
+            const { createConfig } = useManageAdmin(provider);
             const [tx, account] = await createConfig(new BN(LAMPORTS_PER_SOL))
             console.log("just created this", account)
+            console.log("copy the address to the 'CONFIG' variable if you want to use this config.")
+            break;
+        case "new_admin":
+            const { updateAdmin } = useManageAdmin(provider);
+            console.log("update admin", await updateAdmin(new PublicKey(args[1]), new PublicKey(CONFIG)))
+            console.log("change the 'admin' variable to", args[1])
+            break;
+        case "update_fee":
+            const { updateFee } = useManageAdmin(provider)
+            console.log(`update fee to ${args[1]} lamports`, await updateFee(new BN(parseInt(args[1])), new PublicKey(CONFIG)))
             break;
         case "request":
             const verifiedCollectionAddress = web3.Keypair.generate().publicKey;
             const updateAuthority = web3.Keypair.generate().publicKey;
-            const {requestListng} = useListingRequest(provider, new PublicKey(CONFIG))
+            const { requestListng } = useListingRequest(provider, new PublicKey(CONFIG))
             const result = await requestListng({
                 name: "Loquacious Ladybugs",
                 auction_house: web3.Keypair.generate().publicKey,
-                // verified_collection_address: verifiedCollectionAddress!,
+                verified_collection_address: verifiedCollectionAddress!,
                 governance: web3.Keypair.generate().publicKey,
                 collection_update_authority: updateAuthority,
                 meta_data_url: 'http://whatever.org',
                 vanity_url: 'vanity stuff',
                 token_type: "testtype"
             })
-            console.log("made a request", result)
-            console.log("It worked!", result, verifiedCollectionAddress.toBase58(), updateAuthority.toBase58())
+            console.log("It worked!", "Verified Colleciton Address = ", verifiedCollectionAddress.toBase58(), "Update Authority = ", updateAuthority.toBase58())
             break;
         case "approve":
             const {approveListing} = useAdmin(provider, new PublicKey(CONFIG))
@@ -50,7 +62,7 @@ const main = async () => {
         case "deny":
             const {denyListing} = useAdmin(provider, new PublicKey(CONFIG))
             const deny_result = await denyListing(new PublicKey(args[1]));
-            console.log('successfully approved!', deny_result)
+            console.log('successfully denied!', deny_result)
             break;
         case "list_pending":
             const {getAllPendingListings} = useListingQuery(provider, new PublicKey(CONFIG))
@@ -58,7 +70,7 @@ const main = async () => {
             console.log('denied')
 
             for (let list of pending_listings) {
-                console.log('verified collection address', list.verified_collection_address!.toBase58())
+                console.log('verified collection address', list.verified_collection_address!.toBase58(), "update authority", list.collection_update_authority.toBase58())
             }
             break;
         case "list_approved":
@@ -67,7 +79,7 @@ const main = async () => {
             console.log('approved')
 
             for (let list of listings) {
-                console.log('verified collection address', list.verified_collection_address!.toBase58())
+                console.log('verified collection address', list.verified_collection_address!.toBase58(), "update authority", list.collection_update_authority.toBase58())
             }
             break;
         case "is_approved":
@@ -78,7 +90,10 @@ const main = async () => {
         case "has_token":
             const {hasToken} = useListingQuery(provider, new PublicKey(CONFIG))
             console.log('has token', await hasToken(new PublicKey("5zL9T9M6MbMCQ4ZfkH7nwptUhPPCiUfegmjZZq8Gg1YF")))
-
+        case "refund":
+            const { requestListingRefund } = useListingRequest(provider, new PublicKey(CONFIG))
+            console.log('refund requested', await requestListingRefund(new PublicKey(args[1])))
+            break;
     }
 }
 
